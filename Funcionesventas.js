66679bb5a5317921f0037f34a685a6ab33e9ea43
LEONARDOPEREZ <leonardopm@colegioeiffel.edu.mx>
2023-06-22 09:59:53 -0700

    Pago y algunos otros arreglos(paso en lo que reparo el footer)

---
+++ Funcionesventas.js
@@ -0,0 +1,13 @@
+var galletaImagen = document.querySelector(".donation img"); // Variable para la imagen de la galleta
+var galletaImagen = document.querySelector(".donation img"); // Variable para la imagen de la galleta
+projectImages.forEach(function(image) {
+  // Aquí puedes realizar acciones con cada imagen
+  console.log(image.src); // Ejemplo: Obtener la fuente de la imagen
+});
+
+var galletaImagen = document.querySelector(".donation img"); // Variable para la imagen de la galleta
+var galletaImagen = document.querySelector(".donation img"); // Variable para la imagen de la galleta
+projectImages.forEach(function(image) {
+  // Aquí puedes realizar acciones con cada imagen
+  console.log(image.src); // Ejemplo: Obtener la fuente de la imagen
+});
